56d8f9466d8f859c9ce5c2a4541a6db679b1f4f7
Mohamed Elturky <mohamed@example.com>
2026-04-15 07:08:26 -0700

    fix brand details and remove unused api route

--- src/app/brandDetails/[id]/page.tsx
+++ src/app/brandDetails/[id]/page.tsx
@@ -1,5 +1,5 @@
 import ProductCard from "@/app/_compoents/ProductCard/ProductCard";
-import { ProductType } from "@/app/types/route.misr";
+import { ProductType, SingleType } from "@/app/types/route.misr";
 import { Brandcard, BrandDetails } from "@/Servies/routemisr.servies";
 import Link from "next/link";
 import React from "react";
@@ -7,18 +7,11 @@ import React from "react";
 export default async function page(props: { params: { id: string } }) {
   const params = await props.params;
   const id = params.id;
-
-  //  const singlebrand =await BrandDetails(id)
-
-  //  console.log(singlebrand);
-
-  const Brandcards: ProductType[] = await Brandcard(id);
-  console.log(Brandcards);
+  const Brandcards: SingleType | null = await Brandcard(id);
 
   return (
     <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-      
-      {!Brandcards || Brandcards.length === 0 ? (
+      {!Brandcards ? (
         <div className="col-span-full text-center text-gray-500 text-lg">
           No products available for this brand 😢
         </div>
@@ -29,7 +22,6 @@ export default async function page(props: { params: { id: string } }) {
           </Link>
         ))
       )}
-      
     </div>
   );
 }
